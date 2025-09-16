e3e1f55803ddc813e741d1f2db666421ca296c5a
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-09-16 16:52:55 -0300

    3.11 added dist to the backend

--- part3/phoneBook_backend/index.js
+++ part3/phoneBook_backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(express.static("dist"));
 
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
