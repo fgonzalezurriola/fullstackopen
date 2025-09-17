86de4cdfed4b17a74816d94bf5475530ad1f1497
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-09-17 09:29:31 -0300

    feat 3.16 middleware handler

--- part3/phoneBook_backend/index.js
+++ part3/phoneBook_backend/index.js
@@ -6,9 +6,9 @@ const morgan = require("morgan");
 const app = express();
 const Person = require("./models/persons");
 const persons = require("./models/persons");
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(express.static("dist"));
 
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
@@ -21,19 +21,23 @@ app.use(
 );
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons);
-  });
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -44,20 +48,43 @@ app.post("/api/persons", (request, response) => {
   }
 
   const newPerson = new Person({ name, number });
-  newPerson.save().then((savedPerson) => {
-    response.status(201).json(savedPerson);
-  });
+  newPerson
+    .save()
+    .then((savedPerson) => {
+      response.status(201).json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 // 3.15
 app.delete("/api/persons/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then((person) => {
-    if (!person) {
-      return response.status(404).end();
-    }
-    response.status(204).end();
-  });
+  Person.findByIdAndDelete(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end();
+      }
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
