24742523932b8a5c54fe2b9583610db9814d1f0c
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-09-18 20:11:02 -0300

    feat 4.2

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,40 +1,6 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
 
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
