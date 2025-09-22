24eb0a1542c7bd05bd9f94437e06a1713b186795
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-09-22 09:15:59 -0300

    feat 4.11 and 4.12

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -10,6 +10,14 @@ blogsRouter.get('/', (request, response) => {
 blogsRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
 
+  if (blog.title === undefined || blog.url === undefined) {
+    return response.status(400).json({ error: 'title and url are required' })
+  }
+
+  if (blog.likes === undefined) {
+    blog.likes = 0
+  }
+
   blog.save().then((result) => {
     response.status(201).json(result)
   })
--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -22,6 +22,24 @@ describe('get Blog api tests', () => {
       assert(!('_id' in blog))
     })
   })
+
+  test('blog posts have likes', async () => {
+    const likelessBlog = {
+      title: 'likelessBlog',
+      author: 'me',
+      url: 'example.com',
+    }
+
+    await api.post('/api/blogs').send(likelessBlog).expect(201)
+    const response = await api.get('/api/blogs')
+    response.body.forEach((blog) => {
+      assert('likes' in blog)
+      assert(blog.likes >= 0)
+      if (blog.title === 'likelessBlog') {
+        assert(blog.likes === 0)
+      }
+    })
+  })
 })
 
 describe('post Blog api tests', () => {
@@ -31,6 +49,17 @@ describe('post Blog api tests', () => {
     url: 'example.com',
     likes: 7,
   }
+  const titlelessBlog = {
+    author: 'notme',
+    url: 'example.com',
+    likes: 10,
+  }
+  const urllessBlog = {
+    title: 'title',
+    author: 'notme',
+    likes: 30,
+  }
+
   test('create and delete a blog', async () => {
     const initialBlogs = (await api.get('/api/blogs')).body.length
     await api
@@ -40,6 +69,20 @@ describe('post Blog api tests', () => {
       .expect('Content-Type', /application\/json/)
     const endingBlogs = (await api.get('/api/blogs')).body.length
     assert(initialBlogs < endingBlogs)
+  })
+
+  test('blogs without title or url answer with 400 code', async () => {
+    await api
+      .post('/api/blogs')
+      .send(titlelessBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    await api
+      .post('/api/blogs')
+      .send(urllessBlog)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
   })
 })
 
