a6e4db4969e56e82f825c7b59f64a27174f538c3
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-09-21 23:46:17 -0300

    feat 4.10

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -6,7 +6,7 @@ const assert = require('assert')
 
 const api = supertest(app)
 
-describe('Blog api tests', () => {
+describe('get Blog api tests', () => {
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -21,6 +21,25 @@ describe('Blog api tests', () => {
       assert('id' in blog)
       assert(!('_id' in blog))
     })
+  })
+})
+
+describe('post Blog api tests', () => {
+  const testBlog = {
+    title: 'test title',
+    author: 'me',
+    url: 'example.com',
+    likes: 7,
+  }
+  test('create and delete a blog', async () => {
+    const initialBlogs = (await api.get('/api/blogs')).body.length
+    await api
+      .post('/api/blogs')
+      .send(testBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const endingBlogs = (await api.get('/api/blogs')).body.length
+    assert(initialBlogs < endingBlogs)
   })
 })
 
