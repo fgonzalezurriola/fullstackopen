f0b4f5357094c17bdd9d8f86da0ee8fb136a4e93
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-09-21 23:20:08 -0300

    feat 4.9

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -1,15 +1,27 @@
-const { test, after } = require('node:test')
+const { describe, test, after } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const assert = require('assert')
 
 const api = supertest(app)
 
-test('Blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('Blog api tests', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blog posts have id property', async () => {
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach((blog) => {
+      assert('id' in blog)
+      assert(!('_id' in blog))
+    })
+  })
 })
 
 after(async () => {
