2f84b2db611cb69efd2035723b1a143223160851
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-10-03 19:57:44 -0300

    feat 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => {
+import { useState } from 'react'
+
+const Blog = ({ blog, handleLike }) => {
+  const [displayDetails, setDisplayDetails] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  if (displayDetails) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} <button onClick={() => setDisplayDetails(false)}>hide </button>
+        <br />
+        {blog.url}
+        <br />
+        likes {blog.likes} <button onClick={handleLike}>like</button>
+        <br />
+        {blog.author}
+      </div>
+    )
+  }
+
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div style={blogStyle}>
+      {blog.title} | {blog.author}
+      <button onClick={() => setDisplayDetails(true)}>view</button>
     </div>
   )
 }
