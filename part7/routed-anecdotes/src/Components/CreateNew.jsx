f9046af1ca71c0fd868ad1c7f1d4f8845d202b9d
fgonzalezurriola <fgonzalezurriola@gmail.com>
2025-10-10 14:03:28 -0300

    feat 7.6 and fix 7.5

--- part7/routed-anecdotes/src/Components/CreateNew.jsx
+++ part7/routed-anecdotes/src/Components/CreateNew.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks/useField'
 
 const CreateNew = (props) => {
-  const content = useField()
-  const author = useField()
-  const info = useField()
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
@@ -19,9 +19,9 @@ const CreateNew = (props) => {
   }
 
   const handleReset = () => {
-    content.onChange({ target: { value: '' } })
-    author.onChange({ target: { value: '' } })
-    info.onChange({ target: { value: '' } })
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
